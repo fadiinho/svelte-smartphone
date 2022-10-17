948d6417c7aaa0adba3d2bd6a14ed24508c66992
fadiinho <ffadiinho@gmail.com>
2022-10-17 05:30:49 -0300

    feat: add more icons and make better on large screens

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import App from "./App.svelte";
-  import { Cog, Camera, Phone } from "svelte-hero-icons";
+  import { Cog, Camera, Phone, ShoppingCart, InformationCircle } from "svelte-hero-icons";
   import type { TApp } from "../types/Apps";
 
   const apps: TApp[] = [{
@@ -9,18 +9,25 @@
   }, {
     appName: "Camera",
     icon: Camera,
-    className: "col-start-2 col-end-5"
   }, {
     appName: "Phone",
     icon: Phone,
-    className: "col-start-5 col-end-6"
+  }, {
+    appName: "Shopping",
+    icon: ShoppingCart
+  }, {
+    appName: "Info",
+    icon: InformationCircle
   }]
 </script>
 
 <div class="grid grid-cols-5 grid-rows-[repeat(5,_minmax(0,_1fr))] justify-items-center p-2 flex-grow items-center">
   <div class="w-full grid grid-cols-5 grid-rows-1 row-[-1_/_-2] row-end col-span-full justify-items-center items-center">
     {#each apps as app}
-      <App appName={app.appName} icon={app.icon} className={app.className} />
+      <App
+        appName={app.appName}
+        icon={app.icon}
+        className={app.className} />
     {/each}
   </div>
 </div>
--- src/components/Mainframe.svelte
+++ src/components/Mainframe.svelte
@@ -4,7 +4,7 @@
   import Home from "./Home.svelte";
 </script>
 
-<div id="mainframe" class="flex flex-col w-full h-full bg-[url('/src/assets/wallpaper3.jpg')] bg-no-repeat bg-cover">
+<div id="mainframe" class="flex flex-col max-w-lg max-h-lg sm:w-full h-full bg-[url('/src/assets/wallpaper3.jpg')] bg-no-repeat bg-cover">
   <NotificationArea />
   <Home />
   <HomeButtons />
